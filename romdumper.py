dc8651c81f71eabe8cfbf04bd31bb13d64e48018
Tianu Mihai - Cristian <mihai.tianu@gmail.com>
2022-03-31 12:23:26 +0300

    Update romdumper.py
    Switch to argparse and add dump all functionality with rom/definition/output files specified from command line.

--- romdumper.py
+++ romdumper.py
@@ -3,7 +3,7 @@
 #modules imports
 import os
 import sys
-import getopt
+import argparse
 
 # invoke me from anywhere
 current_path = os.path.dirname(sys.argv[0])
@@ -15,63 +15,69 @@ from src import *
 
 #romdrop metadata file
 def_file = 'path/to/romdrop/metadata/l831eg.xml'
-
 #patched rom file
 rom_file = '/path/to/roms/L831EG_Rev_xyz.bin'
-
 #rom_all_text dump file
 dump_file = 'current_rom_dump.txt'
 
-#init
-rd = dumper.RomDumper(rom_file=rom_file, xml_def_file=def_file)
+#verbose level
+verbose = False
 
-#uncomment this line and comment the above for extra output
-#warning verbose output will appear in the dump file
-#rd = dumper.RomDumper(rom_file=rom_file, xml_def_file=def_file, verbose = True)
+def rd_hdl():
+    return dumper.RomDumper(rom_file=rom_file, xml_def_file=def_file, verbose = verbose)
 
-def show_help():
-    print("WARNING - EXPERIMENTAL !\n")
-    print("Usage:")
-    print("./romdumper.py -c 'category' -t 'name'")
-    print("./romdumper.py -l")
-    print("./romdumper.py -a\n")
-    print("Edit romdumper.py and set the paths to your rom and definition!")
+def list_all(args):
+    rd_hdl().list_category_and_name()
 
-def main(argv):
-    category = ''
-    name = ''
-    listall = ''
-    dumpall = ''
+def dump_table(args):
+    rd_hdl().dump_table(args.category, args.name)
+
+def dump_all(args):
+    rd_hdl().dump_all(dump_file)
+
+def dump_all_cli(args):
+    rd = dumper.RomDumper(rom_file=args.rom_file, xml_def_file=args.def_file, verbose = verbose)
+    rd.dump_all(args.output_file)
+
+def main():
+
+    parser = argparse.ArgumentParser(description='romdrop table(s) dumper')
+    parser.set_defaults(func=lambda x: parser.print_help())
+    subparsers = parser.add_subparsers(help='sub-command help')
+    #list all
+    parser_list_all = subparsers.add_parser('list-all', help='list all tables')
+    parser_list_all.set_defaults(func=list_all)
+    #dump table
+    parser_dump_table = subparsers.add_parser('dump-table', help='dump one table by category and name')
+    parser_dump_table.add_argument('-c', action='store', dest = 'category', required = True,
+        help='category containing table, see list-all')
+    parser_dump_table.add_argument('-n', action='store', dest = 'name', required = True,
+        help='table name in category, see list-all')
+    parser_dump_table.set_defaults(func=dump_table)
+    #dump all tables - use script defined rom/def/out files
+    parser_dump_all = subparsers.add_parser('dump-all', help='dump all tables using the rom/def/output files defined inside the script')
+    parser_dump_all.set_defaults(func=dump_all)
+    #dump all tables - use command line arguments for rom/def/output files
+    parser_dump_all_cli = subparsers.add_parser('cli-dump-all', help='dump all tables to file using command line specified parameters for rom/def/outfile')
+    parser_dump_all_cli.add_argument('-r', action='store', dest = 'rom_file', required = True,
+        help='binary rom file')
+    parser_dump_all_cli.add_argument('-d', action='store', dest = 'def_file', required = True,
+        help='xml metadata for the rom binary')
+    parser_dump_all_cli.add_argument('-o', action='store', dest = 'output_file', required = True,
+        help='desired output file')
+    parser_dump_all_cli.set_defaults(func=dump_all_cli)
+
+    #ensure we have all opts specified
     try:
-        opts, args = getopt.getopt(argv,"htlac:t",["category=","table-name=","list-all","dump-all"])
-    except getopt.GetoptError:
-        show_help()
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            show_help()
-        elif opt in ("-c", "--category"):
-            category = arg
-        elif opt in ("-n", "--table-name"):
-            name = arg
-        elif opt in ("-l", "--list-all"):
-            listall = 'listall'
-        elif opt in ("-a", "--dump-all"):
-            dumpall = 'dumpall'
+        options = parser.parse_args()
+    except:
+        sys.exit(1)
 
-    if dumpall:
-        rd.dump_all(dump_file)
-        sys.exit(0)
-    elif listall:
-        rd.list_category_and_name()
-        sys.exit(0)
-    elif category and name:
-        rd.dump_table(category=category, name=name)
-        sys.exit(0)
-    else:
-        print("Too few arguments!")
-        show_help()
+    #do requested action 
+    options.func(options)
+    sys.exit(0)
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main()
 
